b7272f3d9c452c259f8e408cdc9666f4b84fcbd7
Travis Redd <reddtravismatthew@gmail.com>
2021-11-11 10:11:27 -0700

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using FizzBuzz.Controls;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker work = new Worker();
+            work.doWork();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Worker
+    {
+        FizzBuzzControls fbc = new FizzBuzzControls();
+        UserInput ui = new UserInput();
+        Dictionary<string, object> userInput = new Dictionary<string, object>();
+
+        public void doWork()
+        {
+            Console.Write("Enter an upper bound number: ");
+            ui.intMax = Convert.ToInt32(Console.ReadLine());
+            userInput.Add("intMax", ui.intMax);
+
+            Console.Write("Enter the small divisor: ");
+            ui.smallDivisor = Convert.ToInt32(Console.ReadLine());
+            userInput.Add("smallDivisor", ui.smallDivisor);
+
+            Console.Write("Enter the large divisor: ");
+            ui.largeDivisor = Convert.ToInt32(Console.ReadLine());
+            userInput.Add("largeDivisor", ui.largeDivisor);
+
+            Console.Write("Enter the small divisor print statement: ");
+            ui.smallDivisorPrint = Console.ReadLine();
+            userInput.Add("smallDivisorPrint", ui.smallDivisorPrint);
+
+            Console.Write("Enter the large divisor print statement: ");
+            ui.largeDivisorPrint = Console.ReadLine();
+            userInput.Add("largeDivisorPrint", ui.largeDivisorPrint);
+
+            Console.WriteLine(fbc.fizzBuzz(userInput));
+        }
+    }
+}
